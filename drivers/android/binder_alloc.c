dde689c296ee0ca64cf7f3712a1721a55144927e
david7xw <davdavid7xw@gmail.com>
2025-11-14 22:09:14 +0200

    fix assignment of void function to buffer->oneway_spam_suspect

--- drivers/android/binder_alloc.c
+++ drivers/android/binder_alloc.c
@@ -582,7 +582,7 @@ static struct binder_buffer *binder_alloc_new_buf_locked(
 			 * of async space left (which is less than 10% of total
 			 * buffer size).
 			 */
-			buffer->oneway_spam_suspect = debug_low_async_space_locked(alloc, pid);
+			debug_low_async_space_locked(alloc, pid);
 		} else {
 			alloc->oneway_spam_detected = false;
 		}
