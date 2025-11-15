199f5acdf271864bd500065fc36df4d2e4861c1b
david7xw <davdavid7xw@gmail.com>
2025-11-15 12:40:11 +0200

    fix a improperly fixed merge conflict

--- net/ipv6/ip6_input.c
+++ net/ipv6/ip6_input.c
@@ -51,42 +51,12 @@ void udp_v6_early_demux(struct sk_buff *);
 void tcp_v6_early_demux(struct sk_buff *);
 int ip6_rcv_finish(struct net *net, struct sock *sk, struct sk_buff *skb)
 {
-	/* if ingress device is enslaved to an L3 master device pass the
-	 * skb to its handler for processing
-	 */
-	skb = l3mdev_ip6_rcv(skb);
-	if (!skb)
-		return NET_RX_SUCCESS;
+    skb = l3mdev_ip6_rcv(skb);
+    if (!skb)
+        return NET_RX_SUCCESS;
+    ip6_rcv_finish_core(net, sk, skb);
 
-	if (READ_ONCE(net->ipv4.sysctl_ip_early_demux) &&
-	    !skb_dst(skb) && !skb->sk) {
-		switch (ipv6_hdr(skb)->nexthdr) {
-		case IPPROTO_TCP:
-			if (READ_ONCE(net->ipv4.sysctl_tcp_early_demux))
-				tcp_v6_early_demux(skb);
-			break;
-		case IPPROTO_UDP:
-			if (READ_ONCE(net->ipv4.sysctl_udp_early_demux))
-				udp_v6_early_demux(skb);
-			break;
-		}
-	}
-
-	if (!skb_valid_dst(skb))
-		ip6_route_input(skb);
-	return NET_RX_SUCCESS;
-}
-
-{
-	/* if ingress device is enslaved to an L3 master device pass the
-	 * skb to its handler for processing
-	 */
-	skb = l3mdev_ip6_rcv(skb);
-	if (!skb)
-		return NET_RX_SUCCESS;
-	ip6_rcv_finish_core(net, sk, skb);
-
-	return dst_input(skb);
+    return dst_input(skb);
 }
 
 static void ip6_sublist_rcv_finish(struct list_head *head)
